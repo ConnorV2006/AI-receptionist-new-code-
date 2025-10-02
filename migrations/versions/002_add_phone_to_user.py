ce4d74d8419e63056dc8c4037b4ef31a9daa77f3
ConnorV2006 <234860846+ConnorV2006@users.noreply.github.com>
2025-10-02 15:07:03 -0500

    Update 002_add_phone_to_user.py

--- migrations/versions/002_add_phone_to_user.py
+++ migrations/versions/002_add_phone_to_user.py
@@ -3,7 +3,6 @@
 Revision ID: 0002
 Revises: 0001
 Create Date: 2025-10-02
-
 """
 from alembic import op
 import sqlalchemy as sa
@@ -16,8 +15,10 @@ depends_on = None
 
 
 def upgrade():
+    # Add new column phone_number to user table
     op.add_column("user", sa.Column("phone_number", sa.String(length=20)))
 
 
 def downgrade():
+    # Remove column phone_number if rolled back
     op.drop_column("user", "phone_number")
