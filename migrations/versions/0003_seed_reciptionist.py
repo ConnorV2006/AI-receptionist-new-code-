6798f2f47f0dca3b0902df0c30ae9c942e7a863c
ConnorV2006 <234860846+ConnorV2006@users.noreply.github.com>
2025-10-02 23:08:34 -0500

    Update 0003_seed_reciptionist.py

--- migrations/versions/0003_seed_reciptionist.py
+++ migrations/versions/0003_seed_reciptionist.py
@@ -1,4 +1,4 @@
-"""Seed receptionist user"""
+"""Seed receptionist"""
 
 from alembic import op
 import sqlalchemy as sa
@@ -11,12 +11,12 @@ depends_on = None
 
 def upgrade():
     conn = op.get_bind()
-    conn.execute(
-        sa.text("""
-            INSERT INTO "user" (username, email, password, role)
-            VALUES ('reception1', 'reception1@example.com', 'pbkdf2:sha256:260000$recept$hashedpass', 'receptionist')
-        """)
-    )
+    conn.execute(sa.text("""
+        INSERT INTO "user" (username, email, password, role)
+        VALUES ('reception1', 'reception1@example.com',
+        'scrypt:32768:8:1$01NbZGkzRUuUt1v3$09478601114007e5c2b33ad5d30fef52eeecfc75e779f8417fd934854fd3120defb5a480f365a9e684d640e6d6c746c7e893e24eb58d5019e81dd433915f1e1d',
+        'receptionist')
+    """))
 
 
 def downgrade():
